6216b5be1f997bac4f89cc671d739b4b89f63472
Alfredchong726 <alfredchong260@gmail.com>
2023-03-13 21:13:43 +0800

    first commit

---
+++ static/javascript/time.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    function currentTime() {
+    let date = new Date();
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    hh = hh < 10 ? `0${hh}` : hh;
+    mm = mm < 10 ? `0${mm}` : mm;
+    ss = ss < 10 ? `0${ss}` : ss;
+    
+    let time = `${hh}:${mm}:${ss}`;
+    document.querySelector("h1").innerHTML = time;
+    }
+    
+    currentTime();
+    setInterval(currentTime, 1000);
+})
